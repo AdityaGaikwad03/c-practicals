d35971d8313796f4d8e0f098690784eba7a49187
Aditya Gaikwad <128017788+AdityaGaikwad03@users.noreply.github.com>
2024-04-25 20:52:36 +0530

    Update 4_topolocical_sort.c

--- 4_topolocical_sort.c
+++ 4_topolocical_sort.c
@@ -1,43 +1,69 @@
-#include <stdio.h> 
-int main()
-{
-int i,j,k, n, a[10] [10],indeg[10], flag[10], count=0;
-printf("numbers of verticas:\n"); 
-scanf("%d",&n);
-printf ("entes the adjacency mabeine:\n");
-for (i=0;i<n;i++)
-  {
-printf("enter the rows %d\n",i+1); 
-for( j = 0; j < n ;j++)
-scanf("%d",&a[1][j]);
-  }
-for (i=0; i<n; i++)
-{
-indeg[i]=0;
-flag [i]=0;
-}
-for (i=0; i<n; i++)
-for (j=0; j<n; i++)
-  indeg[i]=indeg[i] + a[j][i];
-printf("In The topological sedet is");
+#include <stdio.h>
+#include <stdlib.h>
 
-while (count<n)
-{
-for(k=0;k<n; k++)
-{
-if ((indeg [k] ==0) && (flag[k]= 0));
-{
-printf ("%d", (k + 1) );
-flag [k] = 1;
- break;
+#define MAX_NODES 100
+
+int adj_matrix[MAX_NODES][MAX_NODES];
+int visited[MAX_NODES];
+int sorted_order[MAX_NODES];
+int n;
+
+void initialize() {
+    int i, j;
+    for (i = 0; i < MAX_NODES; i++) {
+        visited[i] = 0;
+        sorted_order[i] = 0;
+        for (j = 0; j < MAX_NODES; j++) {
+            adj_matrix[i][j] = 0;
+        }
+    }
 }
+
+void topologicalSortUtil(int v, int* index) {
+    visited[v] = 1;
+
+    int i;
+    for (i = 0; i < n; i++) {
+        if (adj_matrix[v][i] && !visited[i]) {
+            topologicalSortUtil(i, index);
+        }
+    }
+
+    sorted_order[--(*index)] = v;
 }
-for ( i= 0 ;i<n;i++)
-{
-if (a[k][i]==1)
-indeg[i]--;
+
+void topologicalSort() {
+    int i;
+    int index = n;
+
+    for (i = 0; i < n; i++) {
+        if (!visited[i]) {
+            topologicalSortUtil(i, &index);
+        }
+    }
+
+    printf("Topological Sort Order: ");
+    for (i = 0; i < n; i++) {
+        printf("%d ", sorted_order[i]);
+    }
+    printf("\n");
 }
-count++;
-}
-return 0;
+
+int main() {
+    printf("Enter the number of nodes: ");
+    scanf("%d", &n);
+
+    initialize();
+
+    printf("Enter the adjacency matrix:\n");
+    int i, j;
+    for (i = 0; i < n; i++) {
+        for (j = 0; j < n; j++) {
+            scanf("%d", &adj_matrix[i][j]);
+        }
+    }
+
+    topologicalSort();
+
+    return 0;
 }
